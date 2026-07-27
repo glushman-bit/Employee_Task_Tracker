50b7b104a3dba78e8efe25945f87f94447516c6a
Ivan Glushenkov <glushman966@gmail.com>
2026-07-27 21:44:44 +0300

    create command create_superuser.py

---
+++ users/management/commands/create_superuser.py
@@ -0,0 +1,19 @@
+import os
+
+from django.core.management import BaseCommand
+from users.models import User
+
+
+class Command(BaseCommand):
+    """Команда для создания суперпользователя."""
+
+    help = "Создание суперпользователя"
+
+    def handle(self, *args, **options):
+        user = User.objects.create(email=os.getenv('ADMIN_EMAIL'))
+        user.set_password(os.getenv('ADMIN_PASSWORD'))
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser =True
+        self.stdout.write(self.style.SUCCESS(f'Пользователь <{user.email}> успешно создан.'))
+        user.save()
