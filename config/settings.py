5f3665855cb8994be6857e27a70807771c24b9a2
Ivan Glushenkov <glushman966@gmail.com>
2026-07-28 12:48:50 +0300

    add 'DATETIME_FORMAT' in settings.py

--- config/settings.py
+++ config/settings.py
@@ -106,9 +106,10 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        # 'rest_framework.permissions.AllowAny', # Доступ всем
-        'rest_framework.permissions.IsAuthenticated',  # Доступ только авторизованным
+        'rest_framework.permissions.AllowAny', # Доступ всем
+        # 'rest_framework.permissions.IsAuthenticated',  # Доступ только авторизованным
     ],
+    'DATETIME_FORMAT': '%d.%m.%Y %H:%M',
     # Пагинация на уровне проекта
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 10,
