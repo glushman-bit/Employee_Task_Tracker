d56b4d31ffd65981975fa7b27ca5ac56c7e073b3
Ivan Glushenkov <glushman966@gmail.com>
2026-07-29 17:23:45 +0300

    create admin.py for employees

--- employees/admin.py
+++ employees/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from employees.models import Employee
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    """Административная панель для сотрудников."""
+
+    list_display = ('full_name', 'position', 'owner',)
+    search_fields = ('owner',)
+    list_filter = ('owner', 'position',)
+    ordering = ('position',)
