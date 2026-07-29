f42eafbe54cc0cfc0d0f3ad0ea9160dde79217ca
Ivan Glushenkov <glushman966@gmail.com>
2026-07-29 17:25:19 +0300

    add ViewSet for employees and StatisticsAPIView in views.py

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from employees.models import Employee
+from employees.serializers import EmployeeSerializer, EmployeeCreateSerializer
+
+
+class EmployeeViewSet(ModelViewSet):
+    """Класс работы с сотрудниками."""
+
+    def get_serializer_class(self):
+        """Переопределение сериалайзера в зависимости от действия."""
+
+        if self.action in ['create', 'update', 'partial_update']:
+            return EmployeeCreateSerializer
+
+        return EmployeeSerializer
+
+    def get_queryset(self):
+        """Фильтруем вывод списка сотрудников по руководителю."""
+
+        return Employee.objects.filter(owner=self.request.user)
+
+
+    def perform_create(self, serializer):
+        """Автоматическая установка руководителя при создании."""
+
+        serializer.save(owner = self.request.user)
+
+
+class StatisticsAPIView(APIView):
+    """Класс вывода статистики."""
+
+    def get(self, request):
+        """Вывод количества сотрудников по руководителю."""
+
+        return Response({
+            'employees_count': request.user.employees.count(),
+        })
+
+
+
