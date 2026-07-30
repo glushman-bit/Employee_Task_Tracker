ef6aa52ae29cd5179a42dc098653207eaaa31e32
Ivan Glushenkov <glushman966@gmail.com>
2026-07-30 15:12:23 +0300

    update config/urls.py and employees/urls/py

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,6 @@ from config import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls'), name='users'),
-    path('employees/', include('employees.urls'), name='employees'),
+    path('api/', include('employees.urls'), name='employees'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- employees/urls.py
+++ employees/urls.py
@@ -2,14 +2,18 @@ from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 from employees.apps import EmployeesConfig
-from employees.views import EmployeeViewSet, StatisticsAPIView
+from employees.views import EmployeeViewSet, StatisticsAPIView, TaskViewSet, EmployeeWorkloadAPIView
+
 
 app_name = EmployeesConfig.name
 
 router = SimpleRouter()
-router.register(r'', EmployeeViewSet, basename="employee")
+
+router.register(r'employees', EmployeeViewSet, basename="employee")
+router.register(r'tasks', TaskViewSet, basename="task")
 
 urlpatterns = [
     path('statistics/', StatisticsAPIView.as_view(), name='statistics'),
+    path('statistics/workload/', EmployeeWorkloadAPIView.as_view(), name='workload'),
 ]
 urlpatterns += router.urls
