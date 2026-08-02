67fbce00a6e135f6c98607b05f8743c9e68b0d2b
Ivan Glushenkov <glushman966@gmail.com>
2026-08-02 16:54:13 +0300

    update command super_user

--- users/management/commands/create_superuser.py
+++ users/management/commands/create_superuser.py
@@ -11,10 +11,16 @@ class Command(BaseCommand):
     help = "Создание суперпользователя"
 
     def handle(self, *args, **options):
-        user = User.objects.create(email=os.getenv('ADMIN_EMAIL'))
-        user.set_password(os.getenv('ADMIN_PASSWORD'))
-        user.is_active = True
-        user.is_staff = True
-        user.is_superuser = True
-        self.stdout.write(self.style.SUCCESS(f'Пользователь <{user.email}> успешно создан.'))
-        user.save()
+
+        user, create = User.objects.create(email=os.getenv('ADMIN_EMAIL'))
+        if create:
+            user.set_password(os.getenv('ADMIN_PASSWORD'))
+            user.is_active = True
+            user.is_staff = True
+            user.is_superuser = True
+            self.stdout.write(self.style.SUCCESS(f'Суперпользователь <{user.email}> успешно создан.'))
+            user.save()
+
+        else:
+            self.stdout.write(self.style.SUCCESS(f'Суперпользователь <{user.email}> уже существует.'))
+
